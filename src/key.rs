1784cc0a405702965c20131bc6b84b1787f251dc
jeppeter <jeppeter@gmail.com>
2021-04-03 06:54:24 +0800

    to add Key type items

--- src/key.rs
+++ src/key.rs
@@ -1,1 +1,15 @@
 use serde_json::{Value};
+
+enum Nargs {	
+	String,
+	i32,
+}
+
+pub struct Key {
+	__value :Value,
+	__prefix :String,
+	__flagname :String,
+	__helpinfo :String,
+	__shortflag :String,
+	__nargs :Nargs,
+}
