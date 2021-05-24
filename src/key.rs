2bcad2c310a22432327342d448f354c4789031e9
jeppeter <jeppeter@gmail.com>
2021-05-24 13:20:33 +0800

    pass test_a032

--- src/key.rs
+++ src/key.rs
@@ -2516,4 +2516,25 @@ mod debug_key_test_case {
     	assert!(flags.get_string_v(KEYWORD_SHORTOPT) == "-m");
     	return;
     }
+
+    #[test]
+    fn test_a032() {
+    	let data = r#""+""#;
+    	let jsonv :Value = serde_json::from_str(data).unwrap();
+    	let cmpjsonv :Value = serde_json::from_str("null").unwrap();
+    	let flags :ExtKeyParse = ExtKeyParse::new("","$<numargs>",&jsonv,false,false,false,"--","-",false).unwrap();
+    	assert!(flags.get_string_v(KEYWORD_FLAGNAME) == KEYWORD_DOLLAR_SIGN);
+    	assert!(flags.get_string_v(KEYWORD_PREFIX) == KEYWORD_BLANK);
+    	assert!(flags.get_value_v() == cmpjsonv);
+    	assert!(flags.get_string_v(KEYWORD_TYPE) == KEYWORD_ARGS);
+    	assert!(flags.get_string_v(KEYWORD_HELPINFO) == KEYWORD_BLANK);
+    	assert!(flags.get_nargs_v(KEYWORD_NARGS) == Nargs::Argtype(String::from(KEYWORD_PLUS_SIGN)));
+    	assert!(flags.get_string_v(KEYWORD_SHORTFLAG) == KEYWORD_BLANK);
+    	assert!(flags.get_string_v(KEYWORD_CMDNAME) == KEYWORD_BLANK);
+    	assert!(flags.get_string_v(KEYWORD_FUNCTION) == KEYWORD_BLANK);
+    	assert!(flags.get_string_v(KEYWORD_VARNAME) == "numargs");
+    	__opt_fail_check(&flags);
+    	return;
+    }
+
 }
