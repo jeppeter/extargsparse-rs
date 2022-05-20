971febb0ac85c7e4f7a0e0cd46508412ab24e332
jeppeter <jeppeter@gmail.com>
2022-05-20 09:04:54 +0800

    pass test_a029 ok

--- extargsparse-rs/src/tests.rs
+++ extargsparse-rs/src/tests.rs
@@ -6,7 +6,7 @@ use super::{extargs_log_trace};
 use super::{error_class};
 use super::namespace::{NameSpaceEx};
 use super::key::{ExtKeyParse,KEYWORD_DOLLAR_SIGN,Nargs,KEYWORD_COUNT,KEYWORD_JSONFILE,KEYWORD_HELP,KEYWORD_BOOL,KEYWORD_ARGS,KEYWORD_ATTR,KeyAttr};
-use super::options::{ExtArgsOptions,OPT_PROG,OPT_ERROR_HANDLER};
+use super::options::{ExtArgsOptions,OPT_PROG,OPT_ERROR_HANDLER,OPT_HELP_HANDLER};
 use super::const_value::{ENV_COMMAND_JSON_SET, ENVIRONMENT_SET, ENV_SUB_COMMAND_JSON_SET};
 use std::cell::RefCell;
 use std::sync::Arc;
@@ -1544,5 +1544,49 @@ fn test_a028() {
 	extargs_load_commandline!(parser,loads).unwrap();
 	let oerr = parser.parse_commandline_ex(Some(params.clone()),None,None,None);
 	assert!(oerr.is_err() == true);
+	return;
+}
+
+#[test]
+fn test_a029() {
+	let loads = r#"        {
+            "verbose|v" : "+",
+            "+http" : {
+                "url|u" : "http://www.google.com",
+                "visual_mode|V": false
+            },
+            "$port|p" : {
+                "value" : 3000,
+                "type" : "int",
+                "nargs" : 1 ,
+                "helpinfo" : "port to connect"
+            },
+            "dep" : {
+                "list|l!attr=cc;optfunc=list_opt_func!" : [],
+                "string|s" : "s_var",
+                "$" : "+",
+                "ip" : {
+                    "verbose" : "+",
+                    "list" : [],
+                    "cc" : []
+                }
+            },
+            "rdep" : {
+                "ip" : {
+                    "verbose" : "+",
+                    "list" : [],
+                    "cc" : []
+                }
+            }
+        }"#;
+	before_parser();
+	let s :String = format!("{{ \"{}\" : \"nohelp\" }}", OPT_HELP_HANDLER);
+	let opt = ExtArgsOptions::new(&s).unwrap();
+
+
+	let parser :ExtArgsParser = ExtArgsParser::new(Some(opt.clone()),None).unwrap();
+	extargs_load_commandline!(parser,loads).unwrap();
+	let sarr = get_cmd_help(parser,"");
+	assert!(check_array_equal(sarr.clone(),format_string_array(vec!["no help information"])));
 	return;
 }
