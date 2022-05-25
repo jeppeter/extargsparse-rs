25e2256cf4e7553fa237db11413485b771ae0b35
jeppeter <jeppeter@gmail.com>
2022-05-25 17:19:04 +0800

    to pass test_a044 ok

--- extargsparse_worker/src/tests.rs
+++ extargsparse_worker/src/tests.rs
@@ -2162,3 +2162,28 @@ fn test_a043() {
     assert!(check_array_equal(ns.get_array("subnargs"),format_string_array(vec!["cc", "dd"])));
     return;
 }
+
+#[test]
+fn test_a044() {
+   let loads = r#"        {
+            "verbose|v" : "+",
+            "kernel|K" : "/boot/",
+            "initrd|I" : "/boot/",
+            "encryptfile|e" : null,
+            "encryptkey|E" : null,
+            "setupsectsoffset" : 663,
+            "ipxe" : {
+                "$" : "+"
+            }
+        }"#;
+    before_parser();
+    let optstr :String = format!(r#"{{"{}": true,"{}" : "++", "{}" : "+"}}"#,OPT_PARSE_ALL,OPT_LONG_PREFIX,OPT_SHORT_PREFIX);
+    let optref :ExtArgsOptions = ExtArgsOptions::new(&optstr).unwrap();
+    let params :Vec<String> = format_string_array(vec!["+K", "kernel", "++initrd", "initrd", "cc", "dd", "+E", "encryptkey", "+e", "encryptfile", "ipxe"]);
+    let parser :ExtArgsParser = ExtArgsParser::new(Some(optref.clone()),None).unwrap();
+    extargs_load_commandline!(parser,loads).unwrap();
+    let ns = parser.parse_commandline_ex(Some(params.clone()),None,None,None).unwrap();
+    assert!(ns.get_string("subcommand") == "ipxe");
+    assert!(check_array_equal(ns.get_array("subnargs"),format_string_array(vec!["cc", "dd"])));
+    return;
+}
