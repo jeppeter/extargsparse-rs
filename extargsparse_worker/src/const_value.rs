32ef96fe7b68a2a47b18f5341711781dcb89f114
jeppeter <jeppeter@gmail.com>
2022-06-01 11:05:35 +0800

    add constant value documentation

--- extargsparse_worker/src/const_value.rs
+++ extargsparse_worker/src/const_value.rs
@@ -1,9 +1,15 @@
 
-
+///  command input set ,this is the first
 pub const COMMAND_SET                  :i32 = 10;
+///  sub command json set
 pub const SUB_COMMAND_JSON_SET         :i32 = 20;
+///  command json set
 pub const COMMAND_JSON_SET             :i32 = 30;
+///  environment set like EXTARGSPARSE_JSON variable
 pub const ENVIRONMENT_SET              :i32 = 40;
+///  sub command environment variable set
 pub const ENV_SUB_COMMAND_JSON_SET     :i32 = 50;
+///  command environment variable set
 pub const ENV_COMMAND_JSON_SET         :i32 = 60;
+///  default value at the last
 pub const DEFAULT_SET                  :i32 = 70;
