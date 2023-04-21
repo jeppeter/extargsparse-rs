9ef56040ba92ddcf8563b9937cf2a988dd10f64a
jeppeter <jeppeter@gmail.com>
2023-04-22 07:09:10 +0800

    to add subcmd in getopt

---
+++ examples/getopt/subcmd/src/main.rs
@@ -0,0 +1,63 @@
+use extargsparse_codegen::{extargs_load_commandline,extargs_map_function};
+//use extargsparse_worker::argset::{ArgSetImpl};
+//use extargsparse_worker::{extargs_error_class,extargs_new_error};
+//use extargsparse_worker::{extargs_log_trace};
+use extargsparse_worker::key::{ExtKeyParse};
+use extargsparse_worker::options::{ExtArgsOptions,OPT_PROG};
+//use extargsparse_worker::namespace::NameSpaceEx;
+use extargsparse_worker::funccall::ExtArgsParseFunc;
+use extargsparse_worker::parser::ExtArgsParser;
+
+
+use std::error::Error;
+use lazy_static::lazy_static;
+//use std::sync::Arc;
+//use std::cell::RefCell;
+//use std::any::Any;
+use std::collections::HashMap;
+
+
+
+
+
+
+#[extargs_map_function()]
+fn main() -> Result<(),Box<dyn Error>> {
+    let loads = r#"{
+            "dep" : {
+                "ip" : {
+                	"$" : "*"
+                },
+                "mip" : {
+                	"$" : "*"
+                }
+            },
+            "rdep" : {
+                "ip" : {
+                },
+                "rmip" : {                	
+                }
+            }
+        }"#;
+    let optstr = format!("{{ \"{}\" : \"cmd1\"}}", OPT_PROG);
+    let options = ExtArgsOptions::new(&optstr)?;
+    let parser :ExtArgsParser = ExtArgsParser::new(Some(options.clone()),None)?;
+    extargs_load_commandline!(parser,loads)?;
+    let mut cmds = parser.get_sub_commands_ex("")?;
+    println!("main cmd subcmds:{:?}", cmds);
+
+    cmds = parser.get_sub_commands_ex("dep")?;
+    println!("dep cmd subcmds:{:?}", cmds);
+
+
+    cmds = parser.get_sub_commands_ex("rdep.ip")?;
+    println!("rdep.ip cmd subcmds:{:?}", cmds);
+
+/*
+output:
+main cmd subcmds:["dep", "rdep"]
+dep cmd subcmds:["ip", "mip"]
+rdep.ip cmd subcmds:[]
+*/
+    Ok(())
+}
