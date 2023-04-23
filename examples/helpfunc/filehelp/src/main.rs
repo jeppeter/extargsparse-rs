12df0d42ee57763d942df7b2f8745d6dcc92ead3
jeppeter <jeppeter@gmail.com>
2023-04-23 10:17:51 +0800

    to add filehelp

---
+++ examples/helpfunc/filehelp/src/main.rs
@@ -0,0 +1,83 @@
+use extargsparse_codegen::{extargs_load_commandline,extargs_map_function};
+//use extargsparse_worker::argset::{ArgSetImpl};
+//use extargsparse_worker::{extargs_error_class,extargs_new_error};
+use extargsparse_worker::options::{ExtArgsOptions,OPT_PROG};
+//use extargsparse_worker::namespace::NameSpaceEx;
+use extargsparse_worker::funccall::ExtArgsParseFunc;
+use extargsparse_worker::parser::ExtArgsParser;
+
+
+use std::error::Error;
+use lazy_static::lazy_static;
+//use regex::Regex;
+//use std::sync::Arc;
+//use std::cell::RefCell;
+//use std::any::Any;
+use std::collections::HashMap;
+use std::fs::{File};
+//use std::io::prelude::*;
+
+
+
+#[extargs_map_function()]
+fn main() -> Result<(),Box<dyn Error>> {
+    let cmdline = r#"{
+            "verbose|v" : "+",
+            "+http" : {
+                "url|u" : "http://www.google.com",
+                "visual_mode|V": false
+            },
+            "$port|p" : {
+                "value" : 3000,
+                "type" : "int",
+                "nargs" : 1 ,
+                "helpinfo" : "port to connect"
+            },
+            "dep" : {
+                "list|l" : [],
+                "string|s" : "s_var",
+                "$" : "+",
+                "ip" : {
+                    "verbose" : "+",
+                    "list" : [],
+                    "cc" : []
+                }
+            },
+            "rdep" : {
+                "ip" : {
+                    "verbose" : "+",
+                    "list" : [],
+                    "cc" : []
+                }
+            }
+        }"#;
+    let mut f : File = File::create("help.out")?;
+    let optstr :String = format!("{{\"{}\" : \"cmd1\"}}", OPT_PROG);
+    let options = ExtArgsOptions::new(&optstr)?;
+    let parser :ExtArgsParser = ExtArgsParser::new(Some(options.clone()),None)?;
+    extargs_load_commandline!(parser,cmdline)?;
+    parser.print_help_ex(&mut f,"")?;
+/*
+output:
+cmd1  [OPTIONS] [SUBCOMMANDS] [args...]
+
+ [OPTIONS]
+    --json                 json     
+                    json input file to get the value set
+    --help|-h                       
+                    to display this help information
+    --port|-p              port     
+                    port to connect
+    --http-url|-u          http_url 
+                    http_url set default http://www.google.com
+    --http-visual-mode|-V           
+                    http_visual_mode set true default(False)
+    --verbose|-v           verbose  
+                    count set default 0
+
+[SUBCOMMANDS]
+    [dep]   dep handler  
+    [rdep]  rdep handler 
+*/
+    Ok(())
+}
